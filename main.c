de1db21f8cc070ed45e8f926e2c524725eaa3fdb
JasonWu00 <zwu00@stuy.edu>
2020-01-09 10:40:26 -0500

    more work on main.c

--- main.c
+++ main.c
@@ -1,23 +1,37 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <SDL2/SDL.h>
+#include <SDL2/SDL_timer.h>
+#include <SDL2/SDL_image.h>
 //#include "SDL_stdinc.h"
 
 int main() {
   if (SDL_Init(SDL_INIT_VIDEO) != 0) {
     printf("Error initiating SDL: %s\n", SDL_GetError());
+    SDL_Quit();
+    return 1;
   }
-  else {
-    printf("SDL initiated successfully\n");
-  }
+
+  printf("SDL initiated successfully\n");
 
   SDL_Window* window = SDL_CreateWindow("Battleship", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 1280, 960, 0);
   if (window == NULL) {
     printf("Error creating window: %s\n", SDL_GetError());
+    SDL_Quit();
+    return 1;
   }
-  else {
-    printf("Window created successfully\n");
+
+  printf("Window created successfully\n");
+
+  Uint32 render_flags = SDL_RENDER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC;
+  SDL_Renderer* render = SDL_CreateRender(window, -1, render_flags);
+  if (render == NULL) {
+    printf("Error rendering: %s\n", SDL_GetError());
+    SDL_Quit();
+    return 1;
   }
+
+  //SDL_Surface* surface = IMG_Load();
 
   SDL_Quit();
   return 0;
