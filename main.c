c4d8ebc742dd0669c49a233d7b3586c2c3c01de7
JasonWu00 <zwu00@stuy.edu>
2020-01-08 20:02:50 -0500

    encountered no available video device error

---
+++ main.c
@@ -0,0 +1,16 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "SDL.h"
+#include "SDL_stdinc.h"
+
+int main() {
+  SDL_setenv(SDL_VIDEODRIVER);
+  if (SDL_Init(SDL_INIT_VIDEO) != 0) {
+    printf("Error initiating SDL: %s\n", SDL_GetError());
+  }
+  else {
+    printf("SDL initiated successfully\n");
+  }
+  SDL_Quit();
+  return 0;
+}
