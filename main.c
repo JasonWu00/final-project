99bee66a41adb2157af2bfb78f00436226d4fd71
JasonWu00 <zwu00@stuy.edu>
2020-01-11 18:22:06 -0500

    added sprites, fixed img issue

--- main.c
+++ main.c
@@ -3,7 +3,6 @@
 #include <SDL2/SDL.h>
 #include <SDL2/SDL_timer.h>
 #include <SDL2/SDL_image.h>
-//#include "SDL_stdinc.h"
 
 int main() {
   if (SDL_Init(SDL_INIT_VIDEO) != 0) {
@@ -12,11 +11,13 @@ int main() {
     return 1;
   }
 
+  IMG_Init(IMG_INIT_PNG);
   printf("SDL initiated successfully\n");
 
   SDL_Window* window = SDL_CreateWindow("Battleship", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 1280, 960, 0);
   if (window == NULL) {
     printf("Error creating window: %s\n", SDL_GetError());
+    SDL_DestroyWindow(window);
     SDL_Quit();
     return 1;
   }
@@ -27,12 +28,41 @@ int main() {
   SDL_Renderer* render = SDL_CreateRenderer(window, -1, render_flags);
   if (render == NULL) {
     printf("Error rendering: %s\n", SDL_GetError());
+    SDL_DestroyRenderer(render);
+    SDL_DestroyWindow(window);
     SDL_Quit();
     return 1;
   }
 
-  //SDL_Surface* surface = IMG_Load();
+  SDL_Surface* surface = IMG_Load("battleship-grid.png");
+  if (surface == NULL) {
+    printf("Error making surface: %s\n", SDL_GetError());
+    SDL_DestroyRenderer(render);
+    SDL_DestroyWindow(window);
+    SDL_Quit();
+    return 1;
+  }
 
+  SDL_Texture* texture = SDL_CreateTextureFromSurface(render, surface);
+  SDL_FreeSurface(surface);
+  if (texture == NULL) {
+    printf("Error making texture: %s\n", SDL_GetError());
+    SDL_DestroyRenderer(render);
+    SDL_DestroyWindow(window);
+    SDL_Quit();
+    return 1;
+  }
+
+  SDL_RenderClear(render);
+  SDL_RenderCopy(render, texture, NULL, NULL);
+  SDL_RenderPresent(render);
+
+  SDL_Delay(5000);
+
+  SDL_DestroyTexture(texture);
+  SDL_DestroyRenderer(render);
+  SDL_DestroyWindow(window);
+  IMG_Quit();
   SDL_Quit();
   return 0;
 }
--- makefile
+++ makefile
@@ -1,5 +1,5 @@
 all: main.o
-	gcc -o game main.o `sdl2-config --cflags --libs`
+	gcc -o game main.o `sdl2-config --cflags --libs` -l SDL2 -l SDL2_image
 
 main.o: main.c
 	gcc -c main.c
