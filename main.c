3e88740b6ff09d550433561c49ba0eb4316c7cd6
JasonWu00 <zwu00@stuy.edu>
2020-01-12 22:49:58 -0500

    see devlog, main menu up

--- main.c
+++ main.c
@@ -6,26 +6,28 @@
 
 #define WINDOW_WIDTH (640)
 #define WINDOW_HEIGHT (480)
+#define BUTTON_WIDTH (175)
+#define BUTTON_HEIGHT (40)
 
 int main(int argc, char *argv[]) {
   int sdl_startup_error = SDL_Init(SDL_INIT_VIDEO);
-  if (sdl_startup_error != 0) {
+  if (sdl_startup_error != 0) {//initiates SDL
     printf("Error initiating SDL: %s\n", SDL_GetError());
     SDL_Quit();
     return 1;
   }
 
   int numdrivers = SDL_GetNumVideoDrivers();
-  for (int i=0;i<numdrivers;i++){
+  for (int i=0;i<numdrivers;i++){//debugging for video drivers
     const char *videodriver = SDL_GetVideoDriver(i);
     printf("Driver name: %s\n", videodriver);
   }
   printf("Current video driver: %s\n", SDL_GetCurrentVideoDriver());
 
-  IMG_Init(IMG_INIT_PNG);
+  IMG_Init(IMG_INIT_PNG);//initiates sdl_image
   printf("SDL initiated successfully\n");
 
-  SDL_Window *window = SDL_CreateWindow("Battleship",
+  SDL_Window *window = SDL_CreateWindow("Battleship",//make window
                                         SDL_WINDOWPOS_CENTERED,
                                         SDL_WINDOWPOS_CENTERED,
                                         640, 480,
@@ -65,9 +67,9 @@ int main(int argc, char *argv[]) {
   }
   printf("Render successful\n");
 
-  SDL_Surface* surface = IMG_Load("sprites/battleship-grid.png");
+  SDL_Surface* surface = IMG_Load("sprites/main-menu.png");
   if (surface == NULL) {
-    printf("Error making surface: %s\n", SDL_GetError());
+    //printf("Error making surface: %s\n", SDL_GetError());
     SDL_DestroyRenderer(render);
     SDL_DestroyWindow(window);
     SDL_Quit();
@@ -84,6 +86,15 @@ int main(int argc, char *argv[]) {
     SDL_Quit();
     return 1;
   }
+
+  SDL_Surface* pvp_button_surface = IMG_Load("sprites/pvp.png");
+  SDL_Surface* pve_button_surface = IMG_Load("sprites/pvb.png");
+  SDL_Surface* quitgame_button_surface = IMG_Load("sprites/quit-game.png");
+
+  SDL_Texture* pvp_button_texture = SDL_CreateTextureFromSurface(render, pvp_button_surface);
+  SDL_Texture* pve_button_texture = SDL_CreateTextureFromSurface(render, pve_button_surface);
+  SDL_Texture* quitgame_button_texture = SDL_CreateTextureFromSurface(render, quitgame_button_surface);
+
   printf("Texture made\n");
 
   //SDL_Delay(5000);
@@ -91,9 +102,27 @@ int main(int argc, char *argv[]) {
   SDL_Rect dest;
   dest.w = WINDOW_WIDTH;
   dest.h = WINDOW_HEIGHT;
+  SDL_Rect pvp;
+  pvp.x = WINDOW_WIDTH / 2 - 2 *BUTTON_WIDTH / 3 - 25;
+  pvp.y = 175;
+  pvp.w = BUTTON_WIDTH;
+  pvp.h = BUTTON_HEIGHT;
+  SDL_Rect pve;
+  pve.x = WINDOW_WIDTH / 2 - 2 *BUTTON_WIDTH / 3 - 25;
+  pve.y = 250;
+  pve.w = BUTTON_WIDTH;
+  pve.h = BUTTON_HEIGHT;
+  SDL_Rect quit;
+  quit.x = WINDOW_WIDTH / 2 - 2 * BUTTON_WIDTH / 3 - 25;
+  quit.y = 325;
+  quit.w = BUTTON_WIDTH;
+  quit.h = BUTTON_HEIGHT;
   SDL_QueryTexture(texture, NULL, NULL, &dest.w, &dest.h);
+  SDL_QueryTexture(pvp_button_texture, NULL, NULL, &pvp.w, &pvp.h);
+  SDL_QueryTexture(pve_button_texture, NULL, NULL, &pve.w, &pve.h);
+  SDL_QueryTexture(quitgame_button_texture, NULL, NULL, &quit.w, &quit.h);
 
-  while (1) {
+  while (1) {//loop to prevent window autoclosing
     SDL_Event event;
     while (SDL_PollEvent(&event)) {
       if (event.type == SDL_QUIT) {
@@ -102,6 +131,9 @@ int main(int argc, char *argv[]) {
     }
     SDL_RenderClear(render);
     SDL_RenderCopy(render, texture, NULL, NULL);
+    SDL_RenderCopy(render, pvp_button_texture, NULL, &pvp);
+    SDL_RenderCopy(render, pve_button_texture, NULL, &pve);
+    SDL_RenderCopy(render, quitgame_button_texture, NULL, &quit);
     SDL_RenderPresent(render);
     SDL_Delay(1000/60);
   }
