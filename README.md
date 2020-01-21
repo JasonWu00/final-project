6e87cc12c9e0fa288ccefeb5ee1417670df0e0d0
JasonWu00 <zwu00@stuy.edu>
2020-01-21 16:58:20 -0500

    prelim work on dropping ships

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
 # POINT AND CLICK BATTLESHIP
 
+* WARNING: Incomplete project
+
 # Members:
 Jason Wu, Julian Wong (period 4)
 
@@ -8,7 +10,7 @@ Two-player point-and-click battleship using the SDL library.
 
 # Libraries:
 SDL2:
-enter `sudo apt-get install libsdl2-2.0` and `sudo apt-get install libsdl2-dev`
+enter `sudo apt-get install libsdl2-2.0 libsdl2-dev libsdl2-image-2.0-0 libsdl2-image-dev`
 into the command line.
 The SDL2 library should be installed for the computer once this is done.
 This procedure can take a bit of time.
@@ -17,4 +19,4 @@ Once SDL2 is installed, use the makefile to make and run the game executable fil
 
 # Instructions to use the project:
 
-
+Currently implemented systems contain no actual gameplay.
--- main.c
+++ main.c
@@ -1,3 +1,6 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
 #include <SDL2/SDL.h>
 #include <SDL2/SDL_timer.h>
 #include <SDL2/SDL_image.h>
@@ -10,6 +13,7 @@
 #define BUTTON_HEIGHT (40)
 #define GAME_WIDTH (544)
 #define GAME_HEIGHT (662)
+#define VELOCITY (300)
 
 int main(int argc, char *argv[]) {
 
@@ -69,6 +73,7 @@ int main(int argc, char *argv[]) {
   }
 
   SDL_UpdateWindowSurface(window);
+  SDL_UpdateWindowSurface(game_window);
 
   //SDL_Delay(5000);
 
@@ -110,20 +115,33 @@ int main(int argc, char *argv[]) {
   SDL_Surface* quitgame_button_surface = IMG_Load("sprites/quit-game.png");
   SDL_Surface* surrender_surface = IMG_Load("sprites/surrender.png");
   SDL_Surface* defeated_surface = IMG_Load("sprites/defeated.png");
-  SDL_Surface* aircraft_surface = IMG_Load("sprites/aircraft-placed.png");
-  SDL_Surface* battleship_surface = IMG_Load("sprites/battleship-placed.png");
+  SDL_Surface* carrier_surface = IMG_Load("sprites/carrier-sprite.png");
+  SDL_Surface* battleship_surface = IMG_Load("sprites/battleship-sprite.png");
 
   SDL_Texture* pvp_button_texture = SDL_CreateTextureFromSurface(render, pvp_button_surface);
   SDL_Texture* pve_button_texture = SDL_CreateTextureFromSurface(render, pve_button_surface);
   SDL_Texture* quitgame_button_texture = SDL_CreateTextureFromSurface(render, quitgame_button_surface);
   SDL_Texture* surrender_texture = SDL_CreateTextureFromSurface(game_render, surrender_surface);
   SDL_Texture* defeated_texture = SDL_CreateTextureFromSurface(game_render, defeated_surface);
-  SDL_Texture* aircraft_texture = SDL_CreateTextureFromSurface(game_render, aircraft_surface);
+  SDL_Texture* carrier_texture = SDL_CreateTextureFromSurface(game_render, carrier_surface);
   SDL_Texture* battleship_texture = SDL_CreateTextureFromSurface(game_render, battleship_surface);
 
   printf("Textures made\n");
 
   //SDL_Delay(5000);
+  //initiating SDL_Rects for all the sprites
+  SDL_Rect arizona;
+  arizona.x = 0;
+  arizona.y = 0;
+  arizona.w = 115;
+  arizona.h = 30;
+  SDL_QueryTexture(battleship_texture, NULL, NULL, &arizona.w, &arizona.h);
+  SDL_Rect nimitz;
+  nimitz.x = 0;
+  nimitz.y = 0;
+  nimitz.w = 160;
+  nimitz.h = 40;
+  SDL_QueryTexture(carrier_texture, NULL, NULL, &nimitz.w, &nimitz.h);
   SDL_Rect defeat;
   defeat.x = GAME_WIDTH / 2 - 150;
   defeat.y = GAME_HEIGHT / 2 - 75;
@@ -157,34 +175,35 @@ int main(int argc, char *argv[]) {
   surrender.y = GAME_HEIGHT - 40;
   surrender.w = 40;
   surrender.h = 40;
-  SDL_Rect aircraft;
-  aircraft.w = 108;
-  aircraft.h = 23;
-  SDL_Rect battleship;
-  aircraft.w = 85;
-  aircraft.h = 22;
   SDL_QueryTexture(surrender_texture, NULL, NULL, &surrender.w, &surrender.h);
   SDL_QueryTexture(texture, NULL, NULL, &dest.w, &dest.h);
   SDL_QueryTexture(pvp_button_texture, NULL, NULL, &pvp.w, &pvp.h);
   SDL_QueryTexture(pve_button_texture, NULL, NULL, &pve.w, &pve.h);
   SDL_QueryTexture(quitgame_button_texture, NULL, NULL, &quit.w, &quit.h);
-  SDL_QueryTexture(aircraft_texture, NULL, NULL, &aircraft.w, &aircraft.h);
-  SDL_QueryTexture(battleship_texture, NULL, NULL, &battleship.w, &battleship.h);
 
+  int carrier_deployed = 1;
   int battleship_deployed = 0;
   int cruiser_deployed = 0;
   int destroyer_deployed = 0;
   int gunboat_deployed = 0;
-  int aircraft_deployed = 0;
   int game_over_victory = 0;
   int game_over_defeat = 0;
   int frames_to_close_gamewindow = 0;
+  int ship_dropped = 0;
+  int gametype = 0; //1 for pvp, 2 for pve, 0 for no game
 
   //setting up server and client
   //int c1d = client1setup();//c1d is fildes for client 1 ("server side")
   //int c2d = client2setup();//c2d is fildes for client 2 ("client side")
+  float x_pos = (WINDOW_WIDTH - dest.w) / 2;
+  float y_pos = (WINDOW_HEIGHT - dest.h) / 2;
+  float x_vel = 0;
+  float y_vel = 0;
 
   while (1) {//loop to prevent window autoclosing
+    /*if (carrier_deployed = 0) {
+      current = nimitz;
+    }*/
     SDL_Event event;
       while (SDL_PollEvent(&event)) { //check for events
         if (event.window.windowID == gameplay_id) {
@@ -246,21 +265,22 @@ int main(int argc, char *argv[]) {
                   cruiser_deployed = 0;
                   destroyer_deployed = 0;
                   gunboat_deployed = 0;
-                  aircraft_deployed = 0;
+                  carrier_deployed = 1;
                   game_over_victory = 0;
                   game_over_defeat = 0;
                   frames_to_close_gamewindow = 0;
-                  while(battleship_deployed + cruiser_deployed + destroyer_deployed + gunboat_deployed + aircraft_deployed != 5) {//while user placing boats
+                  gametype = 1;
+                  //while(battleship_deployed + cruiser_deployed + destroyer_deployed + gunboat_deployed + aircraft_deployed != 5) {//while user placing boats
                     SDL_HideWindow(window);
                     SDL_ShowWindow(game_window);
-                    while(aircraft_deployed == 0) {
+                    /*while(aircraft_deployed == 0) {
                       if(event.type == SDL_MOUSEBUTTONDOWN) {
                         aircraft.x = event.button.x - (event.button.x % 32) - 16;
                         aircraft.y = event.button.y - (event.button.y % 26) - 366;
                         aircraft_deployed = 1;
                       }
-                    }
-                    while(battleship_deployed == 0) {
+                    }*/
+                    /*while(battleship_deployed == 0) {
                       if(event.type == SDL_MOUSEBUTTONDOWN) {
                         battleship.x = event.button.x - (event.button.x % 32) - 48;
                         battleship.y = event.button.y - (event.button.y % 26) - 366;
@@ -269,12 +289,12 @@ int main(int argc, char *argv[]) {
                         cruiser_deployed = 1;
                         destroyer_deployed = 1;
                       }
-                    }
-                  }
-                  while(game_over_defeat == 0 || game_over_victory == 0) {//while game is going on
+                    }*/
+                  //}
+                  //if (game_over_defeat == 0 || game_over_victory == 0) {//if started
                     SDL_HideWindow(window);
                     SDL_ShowWindow(game_window);
-                  }
+                //  }
                 }
 
               if ( //click on pve button
@@ -288,25 +308,69 @@ int main(int argc, char *argv[]) {
                   cruiser_deployed = 0;
                   destroyer_deployed = 0;
                   gunboat_deployed = 0;
-                  aircraft_deployed = 0;
+                  carrier_deployed = 1;
                   game_over_victory = 0;
                   game_over_defeat = 0;
                   frames_to_close_gamewindow = 0;
-                  while(battleship_deployed + cruiser_deployed + destroyer_deployed + gunboat_deployed + aircraft_deployed != 5) {//while user placing boats
+                  gametype = 2;
+                  /*while(battleship_deployed + cruiser_deployed + destroyer_deployed + gunboat_deployed + aircraft_deployed != 5) {//while user placing boats
                     SDL_HideWindow(window);
                     SDL_ShowWindow(game_window);
 
-                  }
-                  while(game_over_defeat == 0 || game_over_victory == 0) {//while game is going on
+                  }*/
+                  //while(game_over_defeat == 0 || game_over_victory == 0) {//while game is going on
                     SDL_HideWindow(window);
                     SDL_ShowWindow(game_window);
-                  }
+                  //}
                 }
                 break;
             }
           }
           break;
     }
+
+    int mouse_x, mouse_y;
+    int buttons = SDL_GetMouseState(&mouse_x, &mouse_y);
+
+    int target_x = mouse_x - current.w / 2;
+    int target_y = mouse_y - current.h / 2;
+    float delta_x = target_x - x_pos;
+    float delta_y = target_y - y_pos;
+    float distance = sqrt(delta_x * delta_x + delta_y * delta_y);
+
+    /*SDL_Event dropship;
+    while (SDL_PollEvent(&dropship)) {
+      switch (dropship.type) {
+        case SDL_MOUSEBUTTONDOWN:
+          //if (event.t)
+          ship_dropped = 1;
+          break;
+      }
+    }*/
+
+    if (distance < 5) {
+      x_vel = y_vel = 0;
+    }
+    /*else if (ship_dropped = 1){
+      x_vel = 0;
+      y_vel = 0;
+    }*/
+    else if (buttons & SDL_BUTTON(SDL_BUTTON_LEFT))
+    {
+        x_vel = 0;
+        y_vel = 0;
+    }
+    else{
+      x_vel = delta_x * VELOCITY / distance;
+      y_vel = delta_y * VELOCITY / distance;
+    }
+
+    x_pos += x_vel / 60;
+    y_pos += y_vel / 60;
+
+
+    nimitz.y = (int) y_pos;
+    nimitz.x = (int) x_pos;
 
     SDL_RenderClear(render);
     SDL_RenderCopy(render, texture, NULL, NULL);
@@ -317,11 +381,11 @@ int main(int argc, char *argv[]) {
 
     SDL_RenderClear(game_render);
     SDL_RenderCopy(game_render, game_texture, NULL, NULL);
-    if(aircraft_deployed == 1) {
-      SDL_RenderCopy(game_render, aircraft_texture, NULL, &aircraft);
+    if(carrier_deployed == 1) {
+      SDL_RenderCopy(game_render, carrier_texture, NULL, &nimitz);
     }
     if(battleship_deployed == 1) {
-      SDL_RenderCopy(game_render, battleship_texture, NULL, &battleship);
+      SDL_RenderCopy(game_render, battleship_texture, NULL, &arizona);
     }
     if (game_over_defeat == 1) {
       SDL_RenderCopy(game_render, defeated_texture, NULL, &defeat);
